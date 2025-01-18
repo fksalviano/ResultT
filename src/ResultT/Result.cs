69b5022570f6d2276d52595c32dbd81aac4e5b6e
Fabio Salviano <fksalviano@gmail.com>
2025-01-17 23:30:35 -0300

    feat: add http status code error

--- src/ResultT/Result.cs
+++ src/ResultT/Result.cs
@@ -1,10 +1,13 @@
-﻿namespace ResultT;
+﻿using System.Net;
+
+namespace ResultT;
 
 public partial class Result
 {
     public bool Success { get; }
     public Error? Error { get; }
     public string? ErrorMessage => Error?.Message;
+    public HttpStatusCode? StatusCodeError => Error?.StatusCode;
 
     public Result() => (Success, Error) = (true, null);
     public Result(Error error) => (Success, Error) = (false, error);
@@ -18,4 +21,4 @@ public partial class Result<T> : Result
     public Result(Error error) : base(error) => Value = default;
 }
 
-public record Error(string? Message, Exception? Exception = null);
+public record Error(string? Message, Exception? Exception = null, HttpStatusCode? StatusCode = null);
--- src/ResultT/Results.cs
+++ src/ResultT/Results.cs
@@ -1,10 +1,13 @@
-﻿namespace ResultT;
+﻿using System.Net;
+
+namespace ResultT;
 
 public class Results
 {
     public static Result Ok() => new Result();
     public static Result<T> Ok<T>(T? value) => new Result<T>(value);
     
-    public static Error Error(string? message = null, Exception? exception = null) => new Error(message, exception);
+    public static Error Error(string? message = null, Exception? exception = null, HttpStatusCode? statusCode = null) => new Error(message, exception, statusCode);
+    public static Error Error(string? message, HttpStatusCode? statusCode) => new Error(message, null, statusCode);
     public static Error Error(Exception exception) => new Error(exception.Message, exception);
 }
